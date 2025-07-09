5be54258a4988be0016f9974ad1e2035c6527a1c
katecaplina <katecaplina@yandex-team.ru>
2025-07-09 23:56:00 +0300

    Create train_and_save_model.py

---
+++ lesson-07-project-documentation/train_and_save_model.py
@@ -0,0 +1,6 @@
+from recommender import train_and_save_model
+
+if __name__ == "__main__":
+    print("Обучение модели...")
+    train_and_save_model()
+    print("Модель успешно обучена и сохранена в model.pkl")
