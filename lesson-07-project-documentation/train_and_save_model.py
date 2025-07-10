7d0386ef37ce121031a3525b0d9136e2da8d7160
katecaplina <katecaplina@yandex-team.ru>
2025-07-10 21:41:07 +0300

    Delete lesson-07-project-documentation/train_and_save_model.py

--- lesson-07-project-documentation/train_and_save_model.py
+++
@@ -1,6 +0,0 @@
-from recommender import train_and_save_model
-
-if __name__ == "__main__":
-    print("Обучение модели...")
-    train_and_save_model()
-    print("Модель успешно обучена и сохранена в model.pkl")
