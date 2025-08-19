392c71ba52f8d583c4f2eedd8e7641b506e4b732
varyag18 <varyag18@mail.ru>
2025-08-19 06:43:46 +0300

    Update test_data.py

--- lesson-08-code-review/tests/test_data.py
+++ lesson-08-code-review/tests/test_data.py
@@ -1,6 +1,8 @@
+# Pandas импортирована, но не используется. Плохо влияет на чистоту кода и може привести к проблемам с производительностью.
+
 import pandas as pd
 from src.data import load_data
-
+# Ожидается разделение на 2 строки. Лучшие практики оформления кода
 def test_load_data():
     df = load_data("data/uber.csv")
     assert not df.empty
