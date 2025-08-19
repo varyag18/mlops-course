82f49602fb099c7356a1e6b523f3e27e52c9b2f4
varyag18 <varyag18@mail.ru>
2025-08-19 06:33:01 +0300

    Update data.py

--- lesson-08-code-review/src/data.py
+++ lesson-08-code-review/src/data.py
@@ -1,8 +1,9 @@
 import pandas as pd
+# Нужно сделать отстут не на 1, а на 2 строки. Это правило используется перед декларироваинем функции или класса.
 
 def load_data(path):
     return pd.read_csv(path)
-
+# Нужно сделать отстут не на 1, а на 2 строки. Это правило используется перед декларироваинем функции или класса.
 def split_data(data):
     from sklearn.model_selection import train_test_split
     features = data.drop('fare_amount', axis=1)
